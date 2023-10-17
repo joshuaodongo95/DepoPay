820bb6f7b595a681a6b95bbc5619610a0470ab94
Okello gabriel innocent <okellogabrielinnocent@gmail.com>
2023-10-17 17:47:08 +0300

    Add favicon

--- resources/views/partials/admin.blade.php
+++ resources/views/partials/admin.blade.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
         integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
         crossorigin="anonymous" />
+    <link rel="shortcut icon" href="{{ asset('assets/images/icons/favicon.ico') }}" type="image/x-icon">
+
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     <title>{{ trans('panel.site_title') }}</title>
 
